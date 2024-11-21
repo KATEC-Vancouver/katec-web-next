2fcc3bc73e39ec800ed1774bcf5419c110e61ea1
Noel kim <wkim51@my.bcit.ca>
2024-11-21 09:54:30 -0800

    Add scroll-based background change to Nav component

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,5 +1,5 @@
-'use client';
-import React from 'react';
+"use client";
+import React, { useEffect } from 'react';
 import Link from 'next/link';
 import ThemeToggle from './ThemeToggle';
 import { HiBars3 } from 'react-icons/hi2';
@@ -13,19 +13,40 @@ export const navLinks = [
   { id: 6, url: '#', label: 'Contact' },
 ];
 
-const Nav = () => {
+type Props = {
+  openNav: () => void;
+};
+
+const Nav = ({ openNav }: Props) => {
+  const [navBg, setNavBg] = React.useState(false);
+
+  useEffect(() => {
+    const handler = () => {
+      if (window.scrollY > 100) setNavBg(true);
+      if (window.scrollY < 100) setNavBg(false);
+    };
+
+    window.addEventListener("scroll", handler);
+
+    return () => window.removeEventListener("scroll", handler);
+  }, []);
+
   return (
-    <div className="w-full transition-all duration-200 h-[12vh] z-[1000]">
+    <div
+      className={` fixed ${
+        navBg ? 'bg-blue-100 shadow-md' : 'fixed'
+      } w-full transition-all duration-200 h-[12vh] z-[1000]`}
+    >
       <div className="flex items-center h-full justify-between w-[90%] xl:w-[80%] 2xl:w-[70%] mx-auto">
         {/* Logo */}
         <div className="flex items-center">
           <img
-            src="/images/nonBg-logo.png" 
+            src="/images/nonBg-logo.png"
             alt="Logo"
-            className="h-10 md:h-12" 
+            className="h-10 md:h-12"
           />
         </div>
-        
+
         {/* Nav Links */}
         <div className="hidden lg:flex items-center space-x-10 ml-15">
           {navLinks.map((link) => (
@@ -36,7 +57,10 @@ const Nav = () => {
         </div>
         <ThemeToggle />
         {/* Burger Menu */}
-        <HiBars3 className="w-8 h-8 cursor-pointer text-black lg:hidden " />
+        <HiBars3
+          onClick={openNav}
+          className="w-8 h-8 cursor-pointer text-black lg:hidden "
+        />
       </div>
     </div>
   );
--- components/Home/Navbar/ResNav.tsx
+++ components/Home/Navbar/ResNav.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import Nav from './Nav'
 import MobileNav from './MobileNav'
 
 const ResNav = () => {
+
+  const [showNav, setShowNav] = useState(false)
+  const handleNavShow = () => {
+    setShowNav(true);
+  }
+  const handleNavHide = () => {
+    setShowNav(false);
+  }
+
   return (
     <div>
-      <Nav />
-      <MobileNav />
+      <Nav openNav ={handleNavShow} />
+      <MobileNav showNav={showNav} closeNav={handleNavHide} />
     </div>
   )
 }
