c7078f8da13c38b348d8676b2e463b3a78f75c55
Noel kim <wkim51@my.bcit.ca>
2024-11-22 15:20:03 -0800

    add footer

--- components/Home/Headline/Headline.tsx
+++ components/Home/Headline/Headline.tsx
@@ -13,11 +13,11 @@ const Headline = () => {
               <div className="px-3 py-1 md:px-5 md:py-1 rounded-full bg-green-400 md:text-base sm:text-sm text-xs text-white">
                 Join
               </div>
-              <p className="text-xs sm:text-sm">당신을 카텍의 맴버로 초대합니다!</p>
+              <p className="text-xs sm:text-sm">여러분을 카텍의 맴버로 초대합니다!</p>
             </div>
             {/* Title */}
             <h1 className="text-2xl sm:text-4xl md:text-5xl mt-6 mb-6 font-bold md:leading-[3rem] lg:leading-[3.5rem]">
-              The Katec is The communiy of Korean Developers that has everything you need.
+              The KATEC is The communiy of Korean Developers that has everything you need.
             </h1>
             {/* Description */} 
             <p>
--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,11 +1,13 @@
 
 import React from 'react'
 import Headline from './Headline/Headline'
+import Footer from './Footer/Footer'
 
 const Home = () => {
   return (
     <div className='overflow-hidden'>
       <Headline />  
+      <Footer />
     </div>
   )
 }
