9c6cef79ea406965fcf4a07f081cbe16c6861040
Noel kim <wkim51@my.bcit.ca>
2024-11-23 14:01:31 -0800

    add LogoTicker

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -2,12 +2,13 @@
 import React from 'react'
 import Headline from './Headline/Headline'
 import Footer from './Footer/Footer'
+import LogoTicker from './LogoTicker/LogoTicker'
 
 const Home = () => {
   return (
     <div className='overflow-hidden'>
       <Headline />  
-     
+      <LogoTicker />
       <Footer />
     </div>
   )
---
+++ components/Home/LogoTicker/LogoTicker.tsx
@@ -0,0 +1,58 @@
+'use client'  
+import Image from 'next/image'
+import { motion } from "framer-motion";
+import { Fragment } from 'react';
+
+const logos = [
+  { name : 'logo1', src: '/images/logo1.png'},
+  { name : 'logo2', src: '/images/logo2.png'},
+  { name : 'logo3', src: '/images/logo3.png'},
+  { name : 'logo4', src: '/images/logo4.png'},
+  { name : 'logo5', src: '/images/logo5.png'},
+ 
+  
+];
+
+export default function LogoTicker() {
+  return (
+    <section className="py-6 overflow-x-clip bg-gray-100">
+      <div className="container">
+        <h3 className="text-center text-gray-400 text-xl">
+        Together with our sponsors, shaping a better tech community
+        </h3>
+        <div
+          className="flex overflow-hidden mt-10"
+          style={{
+            maskImage: "linear-gradient(to right, transparent, black, transparent)",
+          }}
+        >
+          <motion.div
+            animate={{
+              x: "-50%",
+            }}
+            transition={{
+              duration: 10,
+              ease: "linear",
+              repeat: Infinity,
+            }}
+            className="flex flex-none gap-24 pr-24"
+          >
+            {Array.from({ length: 2 }).map((_, i) => (
+              <Fragment key={i}>
+                {logos.map((logo) => (
+                  <Image
+                    src={logo.src}
+                    key={logo.name}
+                    alt={logo.name}
+                    width={100} 
+                    height={50} 
+                  />
+                ))}
+              </Fragment>
+            ))}
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
+}
