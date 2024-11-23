6829dddfddbdc6996a7bb680c07bb8eb7db1cd02
Noel kim <wkim51@my.bcit.ca>
2024-11-23 13:03:14 -0800

    finished footer

--- components/Home/Footer/Footer.tsx
+++ components/Home/Footer/Footer.tsx
@@ -1,9 +1,27 @@
 'use client';
 
-const Footer = () => (
-  <footer>
-    footer
-  </footer>
-)
+import Link from 'next/link';
+
+const Footer = () => {
+  return (
+    <footer className="w-full bg-white-800 text-black py-4 bg-gray-100">
+      <div className="flex items-center justify-between max-w-[90%] xl:max-w-[80%] 2xl:max-w-[70%] mx-auto">
+        {/* Logo */}
+        <Link href="/">
+          <img
+            src="/images/nonBg-logo.png"
+            alt="Logo"
+            className="h-11 md:h-12 cursor-pointer"
+          />
+        </Link>
+
+        {/* Text */}
+        <p className="text-sm md:text-base text-gray-400 text-end">
+          2024 KATEC. All Rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -7,6 +7,7 @@ const Home = () => {
   return (
     <div className='overflow-hidden'>
       <Headline />  
+     
       <Footer />
     </div>
   )
