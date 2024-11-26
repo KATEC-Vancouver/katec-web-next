8b8bb0573574935cff399a0767212333d3ec80f4
Noel Kim <wkim51@my.bcit.ca>
2024-11-25 18:29:43 -0800

    add link logo in hero section

--- components/Home/Headline/Headline.tsx
+++ components/Home/Headline/Headline.tsx
@@ -8,12 +8,55 @@ const Headline = () => {
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
           {/* Left */}
           <div>
-            {/* Top box */}
+            {/* Social Links and Invitation */}
             <div className="w-fit py-1.5 px-2 md:px-5 rounded-full shadow-md flex items-center space-x-3 bg-blue-200 dark:bg-lime-400">
-              <div className="px-3 py-1 md:px-5 md:py-1 rounded-full bg-amber-300 md:text-base dark:bg-lime-800 sm:text-sm text-xs text-white">
-                Join
-              </div>
-              <p className="text-xs sm:text-sm ">여러분을 카텍의 맴버로 초대합니다!</p>
+              {/* LinkedIn */}
+              <a
+                href="https://www.linkedin.com/company/katec2024/posts/?feedView=all"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center"
+              >
+                <Image
+                  src="/images/linkdeinlogo.png"
+                  alt="LinkedIn"
+                  width={32}
+                  height={32}
+                  className="rounded-full"
+                />
+              </a>
+              {/* naver*/}
+              <a
+                href="https://cafe.naver.com/katec2024"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center"
+              >
+                <Image
+                  src="/images/naverlogo.png"
+                  alt="Instagram"
+                  width={32}
+                  height={32}
+                  className="rounded-full"
+                />
+              </a>
+              {/* KaKao */}
+              <a
+                href="https://open.kakao.com/o/gIHahNde"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center"
+              >
+                <Image
+                  src="/images/kakaologo.png"
+                  alt="Instagram"
+                  width={32}
+                  height={32}
+                  className="rounded-full"
+                />
+              </a>
+              {/* Invitation Text */}
+              <p className="text-xs sm:text-sm">여러분을 카텍의 맴버로 초대합니다!</p>
             </div>
             {/* Title */}
             <h1
