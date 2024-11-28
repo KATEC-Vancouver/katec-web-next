2eace26922ab899c33405a4ac82c29f8987508f3
Noel Kim <wkim51@my.bcit.ca>
2024-11-28 12:03:03 -0800

    none

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,6 +1,6 @@
 import { Resend } from 'resend';
 import { NextResponse } from 'next/server';
-import { EmailTemplate } from '@/components/email-template';
+import { EmailTemplate } from '@/components/Email/email-template';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
